a443f9a044dec78e806cf6b7ed4930c8d299a464
여왕벌 <s730921@hotmail.com>
2019-07-09 16:19:05 +0900

    초기 커밋.

---
+++ IdeaApp.WebForms/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace IdeaApp.WebForms.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ IdeaApp.WebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdeaApp.WebForms.Startup))]
+namespace IdeaApp.WebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
